a77ecde1a4d2724d9d124dccd0419ee3d46ea82c
Maria Ordonez <mjordonez2001@gmail.com>
2022-11-07 13:19:13 -0500

    Fix login typo

--- src/pages/Pages.tsx
+++ src/pages/Pages.tsx
@@ -6,7 +6,7 @@ const Account = lazy(async () => await import("./account/account"));
 const Cart = lazy(async () => await import("./cart/cart"));
 const Checkout = lazy(async () => await import("./checkout/checkout"));
 const Contact = lazy(async () => await import("./contact/contact"));
-const LogIn = lazy(async () => await import("./log-in/log-in"));
+const Login = lazy(async () => await import("./login/login"));
 const Quiz = lazy(async () => await import("./quiz/quiz"));
 const Shop = lazy(async () => await import("./shop/shop"));
 const Supplement = lazy(async () => await import("./supplement/supplement"));
@@ -20,7 +20,7 @@ function Pages() {
         <Route path="/cart" element={<Cart />} />
         <Route path="/checkout" element={<Checkout />} />
         <Route path="/contact" element={<Contact />} />
-        <Route path="/login" element={<LogIn />} />
+        <Route path="/login" element={<Login />} />
         <Route path="/quiz" element={<Quiz />} />
         <Route path="/shop" element={<Shop />} />
         <Route path="/supplement" element={<Supplement />} />
