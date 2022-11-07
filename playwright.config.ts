84b17b1588a6cda40566362788aad5e1efe55c3b
Maria Ordonez <mjordonez2001@gmail.com>
2022-11-07 09:13:57 -0500

    Remove require dotenv

--- playwright.config.ts
+++ playwright.config.ts
@@ -7,7 +7,6 @@ import type { PlaywrightTestConfig } from "@playwright/test";
  * Read environment variables from file.
  * https://github.com/motdotla/dotenv
  */
-// require('dotenv').config();
 
 /**
  * See https://playwright.dev/docs/test-configuration.
