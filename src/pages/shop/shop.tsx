557f3a384ca32e5b90fe5210d0c9107a447fea45
Maria Ordonez <mjordonez2001@gmail.com>
2022-11-07 09:01:29 -0500

    Make fetch API

--- src/pages/shop/shop.tsx
+++ src/pages/shop/shop.tsx
@@ -1,5 +1,36 @@
+import { useEffect, useState } from "react";
+import { listSupplements } from "../../utils/api";
+
 function Shop() {
-  return <div>Shop</div>;
+  const [supplements, setSupplements] = useState([]);
+  const [supplementsError, setSupplementsError] = useState(null);
+
+  useEffect(() => {
+    const abortController = new AbortController();
+    setSupplementsError(null);
+
+    async function loadSupplements() {
+      listSupplements(abortController.signal)
+        .then(setSupplements)
+        .catch(setSupplementsError);
+    }
+
+    loadSupplements();
+    return () => abortController.abort();
+  }, []);
+
+  console.log(supplements);
+  console.log(supplementsError);
+
+  const allSupplements = supplements.map((supplement) => {
+    console.log(supplement);
+    return (
+      <div key={supplement.id}>
+        {supplement.name}:&nbsp;{supplement.description}
+      </div>
+    );
+  });
+  return <div>{allSupplements}</div>;
 }
 
 export default Shop;
