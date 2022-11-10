70293f221c311a40b9041f699923012dc559c705
Maria Ordonez <mjordonez2001@gmail.com>
2022-11-10 11:37:03 -0500

    Configure axios

--- src/pages/shop/shop.tsx
+++ src/pages/shop/shop.tsx
@@ -1,4 +1,11 @@
+import { axiosClient } from "../../utils/axios";
+
+async function testAPI() {
+  return await axiosClient.get("/test");
+}
+
 function Shop() {
+  testAPI().then(console.log).catch(console.error);
   return <div>Shop</div>;
 }
 
