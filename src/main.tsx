2cdc0195f0a6b5c4f56f69de566cf5908fd47d80
Maria Ordonez <mjordonez2001@gmail.com>
2022-11-10 12:47:36 -0500

    Add unhandled request bypass

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,9 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 
 const { worker } = await import("./mocks/browser");
-await worker.start();
+await worker.start({
+  onUnhandledRequest: "bypass",
+});
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
