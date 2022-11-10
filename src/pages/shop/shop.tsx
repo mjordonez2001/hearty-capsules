e9876e395b1d7963bd002380fd541466aaa2235c
Maria Ordonez <mjordonez2001@gmail.com>
2022-11-10 16:20:07 -0500

    List supplements

--- src/pages/shop/shop.tsx
+++ src/pages/shop/shop.tsx
@@ -1,12 +1,28 @@
+import { useEffect, useState } from "react";
 import { axiosClient } from "../../utils/axios";
 
-async function testAPI() {
-  return await axiosClient.get("/test");
+async function listSupplements() {
+  return await axiosClient.get("/supplements");
 }
 
 function Shop() {
-  testAPI().then(console.log).catch(console.error);
-  return <div>Shop</div>;
+  const [supplements, setSupplements] = useState<
+    Array<{ name: string; slug: string; category: string }>
+  >([]);
+
+  useEffect(() => {
+    listSupplements()
+      .then((response) => setSupplements(response.data))
+      .catch(console.error);
+  }, []);
+
+  return (
+    <div>
+      {supplements.map((supplement) => {
+        return <div key={supplement.slug}>{supplement.name}</div>;
+      })}
+    </div>
+  );
 }
 
 export default Shop;
