db876b0bf18127978efe5253aae587c3dfa148f9
Maria Ordonez <mjordonez2001@gmail.com>
2022-11-17 14:03:53 -0500

    Render supplement

--- src/pages/supplement/supplement.tsx
+++ src/pages/supplement/supplement.tsx
@@ -1,5 +1,20 @@
+import { axiosClient } from "../../utils/axios";
+import { useParams } from "react-router-dom";
+import { useQuery } from "@tanstack/react-query";
+
+async function getSupplement(slug: string) {
+  return await axiosClient.get(`/supplement/${slug}`);
+}
+
 function Supplement() {
-  return <div>Supplement</div>;
+  const params = useParams();
+
+  const query = useQuery(
+    ["supplement", params.slug],
+    async () => await getSupplement(params.slug ?? "")
+  );
+
+  return <div>{query.data?.data.name}</div>;
 }
 
 export default Supplement;
