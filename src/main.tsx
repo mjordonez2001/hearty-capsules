aaa77c1630517f9bb25e77e1a64ee3ead090d3d9
Maria Ordonez <mjordonez2001@gmail.com>
2022-11-17 15:41:05 -0500

    Fix build target, remove top-level await

--- src/main.tsx
+++ src/main.tsx
@@ -2,13 +2,18 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 
-const { worker } = await import("./mocks/browser");
-await worker.start({
-  onUnhandledRequest: "bypass",
-});
+async function prepare() {
+  const { worker } = await import("./mocks/browser");
 
-ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+  await worker.start({
+    onUnhandledRequest: "bypass",
+  });
+
+  ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  );
+}
+
+prepare().catch(console.error);
