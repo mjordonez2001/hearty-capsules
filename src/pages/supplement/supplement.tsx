4663097c89d50fe33cee7bf977df95ae7663417d
Maria Ordonez <mjordonez2001@gmail.com>
2022-11-17 14:34:26 -0500

    Align text

--- src/pages/supplement/supplement.tsx
+++ src/pages/supplement/supplement.tsx
@@ -14,7 +14,28 @@ function Supplement() {
     async () => await getSupplement(params.slug ?? "")
   );
 
-  return <div>{query.data?.data.name}</div>;
+  return (
+    <div className="d-flex m-5 justify-content-evenly">
+      <img src={query.data?.data.photo_url} className="col-5" alt="..." />
+      <div className="px-5 col-6">
+        <div className="fst-italic">{query.data?.data.category}</div>
+        <h2>{query.data?.data.name}</h2>
+        <div>{query.data?.data.description}</div>
+        <div className="my-2">
+          Good for:
+          <ul>
+            {query.data?.data.benefits.map((benefit: string) => {
+              return <li key={benefit}>{benefit}</li>;
+            })}
+          </ul>
+        </div>
+        <div className="fs-3">${query.data?.data.unit_price}</div>
+        <button type="button" className="btn btn-primary my-2">
+          Add to Cart
+        </button>
+      </div>
+    </div>
+  );
 }
 
 export default Supplement;
