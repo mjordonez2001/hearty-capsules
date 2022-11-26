a0f735a4962079aefcdbee3e035be21353d2426d
Maria Ordonez <mjordonez2001@gmail.com>
2022-11-26 17:27:11 -0500

    Add React Icons

--- src/marginals/Header.tsx
+++ src/marginals/Header.tsx
@@ -1,27 +1,34 @@
 import { Link } from "react-router-dom";
+import { SlBag, SlUser } from "react-icons/sl";
 
 function Header() {
   return (
-    <nav className="navbar navbar-expand-md bg-light sticky-top">
+    <nav className="navbar navbar-expand-md bg-light sticky-top px-5">
       <div className="container-fluid">
-        <Link className="navbar-brand mb-0 h1" to="/">
-          Hearty Capsules
-        </Link>
-        <div className="d-flex">
-          <Link className="nav-link mx-2" to="/quiz">
+        <div className="d-flex col-4">
+          <Link className="nav-link mx-3" to="/shop">
+            Shop
+          </Link>
+          <Link className="nav-link mx-3" to="/quiz">
             Quiz
           </Link>
-          <Link className="nav-link mx-2" to="/shop">
-            Products
-          </Link>
-          <Link className="nav-link mx-2" to="/contact">
+          <Link className="nav-link mx-3" to="/contact">
             Contact
           </Link>
-          <Link className="nav-link mx-2" to="/account">
-            Account
+        </div>
+
+        <div className="col-4 d-flex justify-content-center">
+          <Link className="navbar-brand mb-0 h1 fs-3" to="/">
+            Hearty Capsules
           </Link>
-          <Link className="nav-link mx-2" to="/cart">
-            Cart
+        </div>
+
+        <div className="d-flex fs-5 col-4 justify-content-end">
+          <Link className="nav-link mx-3" to="/account">
+            <SlUser />
+          </Link>
+          <Link className="nav-link mx-3" to="/cart">
+            <SlBag />
           </Link>
         </div>
       </div>
