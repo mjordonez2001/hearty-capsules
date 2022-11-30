457c581d7f32ae97599b852099dd580fbc50fd42
Maria Ordonez <mjordonez2001@gmail.com>
2022-11-30 10:54:22 -0500

    Change to footer

--- src/marginals/Footer.tsx
+++ src/marginals/Footer.tsx
@@ -1,10 +1,10 @@
 function Footer() {
   return (
-    <nav className="navbar navbar-expand-md bg-light fixed-bottom">
-      <div className="container-fluid d-flex justify-content-center">
+    <footer className="navbar navbar-expand-md bg-light fixed-bottom">
+      <div className="container-fluid d-flex justify-content-center fw-lighter">
         Hearty Capsules
       </div>
-    </nav>
+    </footer>
   );
 }
 
