41a2babdd3bb23e4c5277a6f6d6acbec4a9a06aa
Maria Ordonez <mjordonez2001@gmail.com>
2022-12-12 13:22:39 -0500

    Map suggested supplements

--- src/pages/supplement/SuggestedProducts.tsx
+++ src/pages/supplement/SuggestedProducts.tsx
@@ -1,5 +1,7 @@
 import { useQuery } from "@tanstack/react-query";
 import { listSuggested } from "../../utils/routes";
+import { supplementProps } from "../../utils/types";
+import Supplement from "../shop/Supplement";
 
 type SuggestedProductsProps = {
   current: string;
@@ -11,10 +13,32 @@ function SuggestedProducts({ current }: SuggestedProductsProps) {
     async () => await listSuggested(current)
   );
 
-  console.log(query.data?.data);
+  if (query.isLoading) {
+    return <div>Loading...</div>;
+  }
+  if (query.isError) {
+    return <div>Something went wrong</div>;
+  }
+
+  const suggested = query.data.data;
+
   return (
-    <div>
-      <div>Suggested Products</div>
+    <div className="d-flex flex-column text-center m-5">
+      <h3>You may also like</h3>
+      <div className="d-flex justify-content-center">
+        {suggested.map((supplement: supplementProps) => {
+          return (
+            <div key={supplement.slug} className="mb-5 mt-3 col-2 mx-3">
+              <Supplement
+                slug={supplement.slug}
+                name={supplement.name}
+                unit_price={supplement.unit_price}
+                photo_url={supplement.photo_url}
+              />
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 }
