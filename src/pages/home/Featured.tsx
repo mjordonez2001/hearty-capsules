2b8f106076b02ab9555e448831b0024d2884be5d
Maria Ordonez <mjordonez2001@gmail.com>
2022-12-14 07:44:51 -0500

    Add backend route for featured products

---
+++ src/pages/home/Featured.tsx
@@ -0,0 +1,7 @@
+import { supplementProps } from "../../utils/types";
+
+function Featured({ name, photo_url, description }: supplementProps) {
+  return <div>Product</div>;
+}
+
+export default Featured;
